ef442d925463a313338e670247e529cdf8e0fd68
QuentYal <90608437+QuentYal@users.noreply.github.com>
2023-01-09 10:54:42 +0100

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'mvc-p1');
+define('DB_PASS', 'flmVrcAN36R6359v');
+define('DB_NAME', 'mvcframework');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'http://mvc-oop-pdo-proeftoets-userstory.com/');
+
+define('SITENAME', 'mvc-p1');
